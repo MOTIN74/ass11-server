aa46f6247e5b0805dc5ddf5763df70e6f7fd0f5c
MD.ABDUL MOTIN <amotin68@gmail.com>
2021-11-01 17:03:44 +0600

    commit

--- index.js
+++ index.js
@@ -1,47 +1,74 @@
-const express = require('express');
 const { MongoClient } = require('mongodb');
-require('dotenv').config();
+const express = require('express');
 const cors = require('cors');
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express()
+require('dotenv').config()
+const ObjectId = require('mongodb').ObjectId
+const port = process.env.PORT || 7000;
 
-// middleware
-app.use(cors());
-app.use(express.json());
 
-const uri = `mongodb+srv://fake_user:5PsL7kuPOX7PZxTz@cluster0.w5gtl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+app.use(cors())
+app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w5gtl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-console.log(uri);
-async function run() {
-    try {
+
+
+const run = async()=>{
+    try{
         await client.connect();
-        const database = client.db('fake_shop');
-        const productCollection = database.collection('fakeProducts');
-    
-        // const orderCollection = database.collection('orders');
+        const database = client.db("tourBD");
+        const touristCollection = database.collection("tourist");
+        const userCollection = database.collection("user");
+        console.log('connected')
 
-        //GET Products API
-        app.get('/products', async (req, res) => {
-           const cursor = productCollection.find({});
-           const products = await cursor.toArray();
-           res.send(products)
-
-    
+        // get api
+        app.get('/card',async(req,res)=>{
+            const cursor = touristCollection.find({})
+            const result = await cursor.toArray()
+            res.send(result)
         })
-   }
-     finally {
-       // await client.close();
+
+        app.post('/card',async(req,res)=>{
+            const newEvent = req.body
+            const result = await touristCollection.insertOne(newEvent)
+            res.json(result)
+        })
+
+        app.post('/users',async(req,res)=>{
+            const newEvent = req.body
+            const result = await userCollection.insertOne(newEvent)
+            res.json(result)
+        })
+
+        app.get('/users',async(req,res)=>{
+            const cursor = userCollection.find({})
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.delete('/users/:id',async(req,res)=>{
+            const id = req.params.id
+            const cursor = {_id : ObjectId(id)}
+            const result = await userCollection.deleteOne(cursor)
+            res.json(result)
+        })
+
     }
- }
+    finally{
 
-run().catch(console.dir);
+    }
+}
 
-app.get('/', (req, res) => {
-    res.send('FakeData server is running');
-});
+run().catch(console.dir)
 
-app.listen(port, () => {
-    console.log('Server running at port', port);
+
+app.get('/',(req,res)=> {
+    res.send('This is home')
+})
+
+app.listen(port,()=>{
+    console.log('port in running on',port)
 })
